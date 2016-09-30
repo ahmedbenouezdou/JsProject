539106e10666e269df3749d49608a338857e7357
Ahmed ben ouezdou <ahmed.b.ouezdou@gmail.com>
2016-09-30 18:22:17 +0200

    Update book.js

--- AngularJS/src/app/book/book.js
+++ AngularJS/src/app/book/book.js
@@ -8,22 +8,22 @@ angular.module('bookModule').controller('bookCrt', function ($scope, BooksListSe
         // $scope.listBook = BooksListService.getList();
         $scope.panier = [];
         $scope.quantite = {};
-
+        $scope.tableView = true;
         BooksListService.setList().then(function (dataBook) {
 
             $scope.panier = OffreService.nombreProduit();
 
             dataBook.forEach(function (elementBook, indexBook) {
                 $scope.quantite[indexBook] = 0;
-                elementBook.disable=true;
+                elementBook.disable = true;
             });
 
-            if($scope.panier.length!=0){
+            if ($scope.panier.length != 0) {
                 dataBook.forEach(function (elementBook, indexBook) {
                     $scope.panier.forEach(function (elementPanier) {
                         if (angular.equals(elementPanier.book.isbn, elementBook.isbn)) {
                             $scope.quantite[indexBook] = elementPanier.quantiteBook;
-                            elementBook.disable=true;
+                            elementBook.disable = true;
                         }
                     })
                 });
@@ -40,39 +40,43 @@ angular.module('bookModule').controller('bookCrt', function ($scope, BooksListSe
     $scope.addPanier = function (index) {
         //index par rapport a la liste des produits et pas par rapport a $rootscope
 
-            var existe = true;
-            var position = 0;
-            for (var j = 0; j < $scope.panier.length; j++) {
-                if (angular.equals($scope.panier[j].book.isbn, BooksListService.getArticle(index).isbn)) {
-                    existe = false;
-                    position = j;
-                }
+        var existe = true;
+        var position = 0;
+        for (var j = 0; j < $scope.panier.length; j++) {
+            if (angular.equals($scope.panier[j].book.isbn, BooksListService.getArticle(index).isbn)) {
+                existe = false;
+                position = j;
             }
-            //si l'article n'existe pas on ajoute dans le panier
-            if (existe) {
-                $scope.panier.push({
-                    'book': BooksListService.getArticle(index),
-                    'quantiteBook': $scope.quantite[index]
-                });
+        }
+        //si l'article n'existe pas on ajoute dans le panier
+        if (existe) {
+            $scope.panier.push({
+                'book': BooksListService.getArticle(index),
+                'quantiteBook': $scope.quantite[index]
+            });
+            $cookieStore.put('listAddPanier', $scope.panier);
+        } else {
+            //sinon si l'article existe modifier quantite
+            if (!angular.equals($scope.panier[position].quantiteBook, $scope.quantite[index])) {
+                $scope.panier[position].quantiteBook = $scope.quantite[index];
                 $cookieStore.put('listAddPanier', $scope.panier);
-            } else {
-                //sinon si l'article existe modifier quantite
-                if (!angular.equals($scope.panier[position].quantiteBook, $scope.quantite[index])) {
-                    $scope.panier[position].quantiteBook = $scope.quantite[index];
-                    $cookieStore.put('listAddPanier', $scope.panier);
 
-                }
             }
+        }
 
     };
 
-    $scope.activePanier=function activePanier(index){
+    $scope.activePanier = function activePanier(index) {
         if ($scope.quantite[index] === 0) {
-            $scope.listBook[index].disable=true;
-        }else{
-            $scope.listBook[index].disable=false;
+            $scope.listBook[index].disable = true;
+        } else {
+            $scope.listBook[index].disable = false;
 
         }
+    };
+
+    $scope.trier = function trier() {
+        $scope.tableView=!$scope.tableView;
     };
     init();
 });
@@ -86,7 +90,7 @@ angular.module('bookModule').service('BooksListService', function ($http, $q) {
         var deferred = $q.defer();
 
         $http.get('listBook/').then(function (data) {
-                listBookServer=data.data
+                listBookServer = data.data
                 deferred.resolve(data.data);
             },
             function (httpError) {
