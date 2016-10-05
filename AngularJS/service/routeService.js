935f0235b2603414a64491383d975897c063f137
Ahmed ben ouezdou <ahmed.b.ouezdou@gmail.com>
2016-10-05 17:46:38 +0200

    Update routeService.js

--- AngularJS/service/routeService.js
+++ AngularJS/service/routeService.js
@@ -20,37 +20,55 @@ exports.getAllList = function getAllList(req, response) {
             "isbn": "a460afed-e5e7-4e39-a39d-c885c05db861",
             "title": "Harry Potter et la Chambre des secrets",
             "price": 30,
-            "cover": "http://localhost:8085/img/hp1.jpg"
+            "cover": "http://localhost:8085/img/hp1.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "Une rentrée fracassante en voiture volante, une étrange malédiction qui s’abat sur les élèves, cette deuxième année à l’école des sorciers ne s’annonce pas de tout repos ! Une rentrée fracassante en voiture volante, une étrange malédiction qui s'abat sur les élèves, cette deuxième année à l'école des sorciers ne s'annonce pas de tout repos ! Entre les cours de potions magiques, les matches de Quidditch et les combats de mauvais sorts, Harry et ses amis Ron et Hermione trouveront-ils le temps de percer le mystère de la Chambre des Secrets ? Le deuxième volume des aventures de Harry Potter : un livre magique pour sorciers confirmés."
         },
         {
             "isbn": "fcd1e6fa-a63f-4f75-9da4-b560020b6acc",
             "title": "Harry Potter et le Prisonnier d'Azkaban",
             "price": 30,
-            "cover": "http://localhost:8085/img/hp2.jpg"
+            "cover": "http://localhost:8085/img/hp2.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "Sirius Black, le dangereux criminel qui s'est échappé de la forteresse d'Azkaban, recherche Harry Potter. C'est donc sous bonne garde que l'apprenti sorcier fait sa troisième rentrée à Poudlard. Au programme : des cours de divination, la fabrication d'une potion de ratatinage, le dressage des hippogriffes... Mais Harry est-il vraiment à l'abri du danger qui le menace ?"
         },
         {
             "isbn": "c30968db-cb1d-442e-ad0f-80e37c077f89",
             "title": "Harry Potter et la Coupe de feu",
             "price": 29,
-            "cover": "http://localhost:8085/img/hp3.jpg"
+            "cover": "http://localhost:8085/img/hp3.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "Harry Potter a quatorze ans et entre en quatrième année au collège de Poudlard. Une grande nouvelle attend Harry, Ron et Hermione à leur arrivée : la tenue d'un tournoi de magie exceptionnel entre les plus célèbres écoles de sorcellerie. Déjà les délégations étrangères font leur entrée. Harry se réjouit... Trop vite. Il va se trouver plongé au cœur des évènements les plus dramatiques qu'il ait jamais eu à affronter."
         },
         {
             "isbn": "78ee5f25-b84f-45f7-bf33-6c7b30f1b502",
             "title": "Herry Potter et l'Ordre du phénix",
             "price": 28,
-            "cover": "http://localhost:8085/img/hp4.jpg"
+            "cover": "http://localhost:8085/img/hp4.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "À quinze ans, Harry entre en cinquième année à Poudlard mais il n'a jamais été aussi anxieux. L'adolescence, la perspective des examens et ces étranges cauchemars... Car Celui-Dont-On-Ne-Doit-Pas-Prononcer-Le-Nom est de retour. Le ministère de la Magie ne semble pas prendre cette menace au sérieux, contrairement à Dumbledore, le directeur du collège de Poudlard. La résistance s'organise alors autour de Harry qui va devoir compter sur le courage et la fidélité de ses amis de toujours..."
         },
         {
             "isbn": "cef179f2-7cbc-41d6-94ca-ecd23d9f7fd6",
             "title": "Harry Potter et le Prince de sang-mélé",
             "price": 30,
-            "cover": "http://localhost:8085/img/hp5.jpg"
+            "cover": "http://localhost:8085/img/hp5.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "Dans un monde de plus en plus inquiétant, Harry se prépare à retrouver Ron et Hermione. Bientôt, ce sera la rentrée à Poudlard, avec les autres étudiants de sixième année. Mais pourquoi Dumbledore vient-il en personne chercher Harry chez les Dursley ? Dans quels extraordinaires voyages au cœur de la mémoire va-t-il l'entraîner ?"
         },
         {
             "isbn": "bbcee412-be64-4a0c-bf1e-315977acd924",
             "title": "Harry Potter et les Reliques de la Mort",
             "price": 35,
-            "cover": "http://localhost:8085/img/hp6.jpg"
+            "cover": "http://localhost:8085/img/hp6.jpg",
+            auteur: "J.K. Rowling",
+            genre: "Roman Fantasy",
+            resume: "Cette année, Harry a dix-sept ans et ne retourne pas à Poudlard. Avec Ron et Hermione, il se consacre à la dernière mission confiée par Dumbledore. Mais le Seigneur des Ténèbres règne en maître. Traqués, les trois fidèles amis sont contraints à la clandestinité. D'épreuves en révélations, le courage, le choix et les sacrifices de Harry seront déterminants dans la lutte contre les forces du Mal."
         }
     ];
 
